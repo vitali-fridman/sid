247e6c854de5c66cc631b86cdf9bab830076a8fc
Vitali Fridman <vitali.fridman@gmail.com>
2015-08-14 18:10:51 -0700

    Next

--- src/test/java/com/shn/dlp/sid/tools/GenerateCensusTestDataTest.java
+++ src/test/java/com/shn/dlp/sid/tools/GenerateCensusTestDataTest.java
@@ -40,5 +40,29 @@ public class GenerateCensusTestDataTest {
 		}
 		
 	}
+	
+	@Test
+	public void testMainWithClearPrint() {
+		String tempDirectory = System.getProperty("java.io.tmpdir");
+		ClassLoader classLoader = this.getClass().getClassLoader();
+		InputStream is = classLoader.getResourceAsStream(FILE_NAME);
+		try {
+			Files.copy(is, Paths.get(tempDirectory, GOLD_FILE_NAME), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			fail("Error creating Gold File in temporary directory.");
+		}
+		
+		String[] args = new String[] {"-f", tempDirectory + "/" + SHORT_FILE_NAME, "-c", "5", "-r", "100000", "-print"};
+		GenerateCensusTestData.main(args);
+		
+		File goldFile = new File(tempDirectory, GOLD_FILE_NAME);
+		File generatedFile = new File(tempDirectory, FILE_NAME);
+		try {
+			assertTrue(com.google.common.io.Files.equal(goldFile, generatedFile));
+		} catch (IOException e) {
+			fail("Error comparing files. " + e.getMessage());
+		}
+		
+	}
 
 }
