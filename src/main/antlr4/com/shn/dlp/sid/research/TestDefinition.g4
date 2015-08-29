77b50c53357bc31f705966889cf08b344ac8959e
Vitali Fridman <vitali.fridman@gmail.com>
2015-08-28 20:53:26 -0700

    Next

--- src/main/antlr4/com/shn/dlp/sid/research/TestDefinition.g4
+++ src/main/antlr4/com/shn/dlp/sid/research/TestDefinition.g4
@@ -1,4 +1,5 @@
 grammar TestDefinition;
 init :  ;
+INT1 : [1][0-9]+ ;
 INT : [0-9]+ ;
 WS : [ \t\r\n]+ -> skip ;
--- src/main/java/com/shn/dlp/sid/research/Antlr4Test.java
+++ src/main/java/com/shn/dlp/sid/research/Antlr4Test.java
@@ -3,6 +3,7 @@ package com.shn.dlp.sid.research;
 import java.util.List;
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.pattern.RuleTagToken;
 
 
 public class Antlr4Test {
@@ -11,12 +12,17 @@ public class Antlr4Test {
 	  {
 		ANTLRFileStream input  = new ANTLRFileStream(args[0]);
 		TestDefinitionLexer lexer = new TestDefinitionLexer(input);
-		while(true) {
+		
+		Vocabulary vc = lexer.getVocabulary();
+		
+		while(!lexer._hitEOF) {
 			Token token = lexer.nextToken();
+			int type = token.getType();
+			System.out.println("Display Name: " + vc.getDisplayName(type));
+			System.out.println("Literal Name: " + vc.getLiteralName(type));
+			System.out.println("Symbolic Name: " + vc.getSymbolicName(type));
+			System.out.println("Text: " + token.getText());
 			System.out.println(token);
-			if (lexer._hitEOF) {
-				break;
-			}
 		}
 	    
 	  }
