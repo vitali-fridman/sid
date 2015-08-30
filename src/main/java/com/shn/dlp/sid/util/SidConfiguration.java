cac89ef052b073b6e56c3be90fb2933a68377cba
Anna <annagam@gmail.com>
2015-08-30 15:32:07 -0700

    Removed java 8 only include.

--- src/main/java/com/shn/dlp/sid/util/SidConfiguration.java
+++ src/main/java/com/shn/dlp/sid/util/SidConfiguration.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
-import java.util.Optional;
 import java.util.Properties;
 
 import org.slf4j.Logger;
