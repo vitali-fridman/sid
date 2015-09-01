3dba1ce4e7b8de5137e631f2b72cff9f6ff65f83
Vitali Fridman <vitali@skyhighnetworks.com>
2015-09-01 15:56:02 -0700

    Next.

--- src/main/java/com/shn/sid/search/SearchIndex.java
+++ src/main/java/com/shn/sid/search/SearchIndex.java
@@ -7,17 +7,22 @@ import java.util.ArrayList;
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shn.dlp.sid.entries.CellRowAndColMask;
+import com.shn.dlp.sid.entries.CellRowAndColMaskListSerializer;
 import com.shn.dlp.sid.entries.RawTerm;
+import com.shn.dlp.sid.entries.RawTermSerializer;
 import com.shn.dlp.sid.entries.TermAndRow;
+import com.shn.dlp.sid.entries.TermAndRowSerializer;
 import com.shn.dlp.sid.entries.Token;
 import com.shn.dlp.sid.indexer.CryptoFileIndexer;
 import com.shn.dlp.sid.indexer.CryptoFileIndexerWorker;
 import com.shn.dlp.sid.indexer.IndexDescriptor;
+import com.shn.dlp.sid.security.CryptoException;
 import com.shn.dlp.sid.util.SidConfiguration;
 
 public class SearchIndex {
@@ -45,25 +50,54 @@ public class SearchIndex {
 		return indexName;
 	}
 	
-	public void openIndex() throws JsonParseException, JsonMappingException, IOException {
+	public void openIndex() throws JsonParseException, JsonMappingException, IOException, CryptoException {
 		File descriptorFile = new File(indexDirectory + File.separator + CryptoFileIndexer.DESCRIPTOR_FILE_NAME);
 		ObjectMapper mapper = new ObjectMapper();
 		this.descriptor = mapper.readValue(descriptorFile, IndexDescriptor.class);
 		this.numShards = descriptor.getNumShards();
 		File commonTermsDBfile = new File(indexDirectory + File.separator+ CryptoFileIndexer.COMMON_TERMS_DB_NAME);
 		this.commonTermsDB = DBMaker.fileDB(commonTermsDBfile).fileMmapEnable().readOnly().make();
-		this.commonTermsMap = this.commonTermsDB.hashMap(CryptoFileIndexer.COMMON_TERMS_MAP_NAME);
+		this.commonTermsMap = this.commonTermsDB.hashMap(CryptoFileIndexer.COMMON_TERMS_MAP_NAME, 
+				new TermAndRowSerializer(config), 
+				Serializer.INTEGER,
+				null);
 		
 		for (int i=0; i<this.numShards; i++) {
-			File unCommonTermsDBfile = new File(indexDirectory + File.separator + 
+			File unCommonTermsDBfile = new File(this.indexDirectory + File.separator + 
 					CryptoFileIndexerWorker.UNCOMMON_TERMS_DB_NAME + "." + i);
 			this.uncommonTermsDBs[i] = DBMaker.fileDB(unCommonTermsDBfile).fileMmapEnable().readOnly().make();
+			this.unCommonTermsMaps[i] = this.uncommonTermsDBs[i].hashMap(CryptoFileIndexerWorker.UNCOMMON_TERMS_MAP_NAME,
+					new RawTermSerializer(this.config),
+					new CellRowAndColMaskListSerializer(),
+					null);
+			File allCommonTermsDBfile = new File(this.indexDirectory + File.separator +
+					CryptoFileIndexerWorker.ALL_COMMON_TERMS_DB_NAME + "." + i);
+			this.allCmmonTermsDBs[i] = DBMaker.fileDB(allCommonTermsDBfile).fileMmapEnable().readOnly().make();
+			this.allCommonTermsMaps[i] = this.allCmmonTermsDBs[i].hashMap(CryptoFileIndexerWorker.ALL_COMMON_TERMS_MAP_NAME, 
+					new RawTermSerializer(this.config), Serializer.INTEGER, null);
 		}
 		
 	}
 	
+	public void closeIndex() {
+		this.commonTermsDB.close();
+		for (int i=0; i<this.numShards; i++) {
+			this.uncommonTermsDBs[i].close();
+			this.allCmmonTermsDBs[i].close();
+		}
+	}
+	
 	public Token.Presense lookupPresense(Token token) {
-		
+		int shard = calculateShard(token);
+		this.allCommonTermsMaps[shard].containsKey(token.getTerm());
+	}
+
+	private int calculateShard(Token token) {
+		RawTerm rt = token.getTerm();
+		int hash = rt.hashCode();
+		int positiveHash = (hash < 0 ? -hash : hash);
+		int shardIndex = positiveHash / (Integer.MAX_VALUE / this.numShards);
+		return shardIndex;
 	}
 	
 	
